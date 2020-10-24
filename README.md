b834c8d45946aacaf4c7780d454fea41a85b6314
Tim Buchholz <buchholz.tim@gmx.de>
2020-10-24 16:54:15 +0200

    Nuxt & VSCode Setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MTI-Projekt
+
 Whiteboard für MTI
+
+# Setup für Projekt
+
+Wir nutzen yarn: https://classic.yarnpkg.com/en/docs/install/#windows-stable
+
+Als Line-Endings wird LF genutzt. Dafür ist die .gitattributes zuständig.
+Falls beim committen Fehler der Art "LF will be converted to CRLF" auftreten muss "git config --global core.autocrlf false" ausgeführt werden
---
+++ frontend/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'airbnb-base',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'off',
+    eqeqeq: 'error',
+    quotes: [2, 'single', { avoidEscape: true }],
+    'linebreak-style': ['error', 'unix'],
+    'prettier/prettier': 0,
+    'operator-linebreak': ['error', 'after'],
+    'import/no-unresolved': 'off', // Conflicts With Nuxt-Shortcut-Imports @,~,
+    'import/extensions': 'off', // Conflicts with Nuxt
+  },
+  globals: {
+    $nuxt: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2017,
+  },
+};
