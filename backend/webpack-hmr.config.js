2e86e981e2d9846aff76b9aca2697f85315085d6
Marc Bollmann <contact@marcbollmann.de>
2020-12-01 10:39:47 +0100

    Added hot-reload when starting application with yarn run start:dev

---
+++ backend/webpack-hmr.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const StartServerPlugin = require('start-server-webpack-plugin');
+
+module.exports = function (options) {
+    return {
+        ...options,
+        entry: ['webpack/hot/poll?100', options.entry],
+        watch: true,
+        externals: [
+            nodeExternals({
+                allowlist: ['webpack/hot/poll?100'],
+            }),
+        ],
+        plugins: [
+            ...options.plugins,
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.WatchIgnorePlugin([/\.js$/, /\.d\.ts$/]),
+            new StartServerPlugin({ name: options.output.filename }),
+        ],
+    };
+};
