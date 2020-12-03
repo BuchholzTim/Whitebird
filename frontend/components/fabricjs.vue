5292bcb02ddd9f22fa89bf05e19c308330b2ec92
demo <demo7996@yahoo.de>
2020-12-03 13:44:33 +0100

    new button

--- frontend/components/fabricjs.vue
+++ frontend/components/fabricjs.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <canvas id="canvas" @mousemove="updateCoordinates"> </canvas>
+  <div @mousemove="updateCoordinates">
+    <canvas id="canvas" width="1200px" height="850px">  </canvas> <br />
     <button @click="rectangleTool">Rectangle</button>
-
-      Coordinates: {{ x }} / {{ y }} <br />
+    <button @click="text">Text</button>
+    <button @click="changeDrawingMode">Drawing</button>
+    <button @click="clear">Clear</button>
+    <br /> Coordinates: {{ x }} / {{ y }} <br />
   </div>
 </template>
 
@@ -12,6 +14,7 @@ import { fabric } from 'fabric';
 
 let width;
 let height;
+const canvas = null;
 
 if (process.client) {
   width = window.innerWidth;
@@ -20,7 +23,7 @@ if (process.client) {
 export default {
 
   data: () => ({
-    srectangleTool: null,
+    drawingMode: false,
     points: {
       from: null,
       to: null,
@@ -31,15 +34,13 @@ export default {
   mounted() {
     console.log('Component created!');
     this.canvas = new fabric.Canvas('canvas');
-
-    console.log(JSON.stringify(this.canvas));
   },
 
   methods: {
     rectangleTool(event) {
       const rect = new fabric.Rect({
-        left: 0,
-        top: 0,
+        left: width / 2,
+        top: height / 2,
         width: 150,
         height: 120,
         fill: 'red',
@@ -48,12 +49,34 @@ export default {
       this.canvas.renderAll();
       console.log(JSON.stringify(this.canvas));
     },
+    text(event) {
+      const text = new fabric.Textbox('hello world', { left: 100, top: 100, lockScalingY: true });
+      this.canvas.add(text).setActiveObject(text);
+
+      this.canvas.add(text);
+    },
+
+    changeDrawingMode(event) {
+      console.log(this.drawingMode = !this.drawingMode);
+      this.canvas.isDrawingMode = this.drawingMode;
+    },
+
     updateCoordinates(event) {
     // pass event object, bound to mouse move with updat
       this.x = event.clientX;
       this.y = event.clientY;
     },
 
+    clear(event) {
+      this.canvas.clear();
+    },
+
   },
 };
 </script>
+
+<style>
+#canvas {
+  border: 1px solid;
+}
+</style>
