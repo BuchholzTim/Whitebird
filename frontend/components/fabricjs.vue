d224168f84ce0689b0168efe2066369766d9b927
Tim Buchholz <buchholz.tim@gmx.de>
2020-12-05 16:48:47 +0100

    Rectangle Shadow

--- frontend/components/fabricjs.vue
+++ frontend/components/fabricjs.vue
@@ -1,16 +1,19 @@
 <template>
   <div @mousemove="updateCoordinates">
-    <canvas id="canvas" width="1200px" height="850px">  </canvas> <br />
+    <canvas id="canvas" width="1200px" height="850px"> </canvas> <br />
     <button @click="rectangleTool">Rectangle</button>
     <button @click="text">Text</button>
     <button @click="changeDrawingMode">Drawing</button>
     <button @click="clear">Clear</button>
-    <br /> Coordinates: {{ x }} / {{ y }} <br />
+    <br />
+    Coordinates: {{ x }} / {{ y }} <br />
   </div>
 </template>
 
 <script>
 import { fabric } from 'fabric';
+import { v4 } from 'uuid';
+import { log } from 'util';
 
 let width;
 let height;
@@ -21,7 +24,6 @@ if (process.client) {
   height = window.innerHeight;
 }
 export default {
-
   data: () => ({
     drawingMode: false,
     points: {
@@ -47,22 +49,51 @@ export default {
       });
       this.canvas.add(rect).setActiveObject(rect);
       this.canvas.renderAll();
-      console.log(JSON.stringify(this.canvas));
     },
     text(event) {
-      const text = new fabric.Textbox('hello world', { left: 100, top: 100, lockScalingY: true });
-      this.canvas.add(text).setActiveObject(text);
+      const shadow = new fabric.Shadow({
+        color: 'rgb(38, 50, 56)',
+        blur: 6,
+        // offsetX: 5,
+        offsetY: 5,
+      });
 
+      const text = new fabric.Textbox('hello world', {
+        left: 100,
+        top: 100,
+        lockScalingY: true,
+        fill: 'rgb(255,255,255)',
+        fontFamily: 'Arial',
+        mtiID: v4(),
+      });
+
+      const rect = new fabric.Rect({
+        left: 85,
+        top: 85,
+        width: text.width + 30,
+        height: text.height + 30,
+        fill: 'rgb(55, 71, 79)',
+        shadow,
+        mtiID: v4(),
+      });
+      this.canvas.add(rect);
       this.canvas.add(text);
+      this.canvas.renderAll();
     },
 
     changeDrawingMode(event) {
-      console.log(this.drawingMode = !this.drawingMode);
+      const obj = this.canvas.getObjects();
+      console.log(obj);
+      obj.forEach((el) => {
+        // eslint-disable-next-line no-underscore-dangle
+        console.log(el.mtiID);
+      });
+      console.log((this.drawingMode = !this.drawingMode));
       this.canvas.isDrawingMode = this.drawingMode;
     },
 
     updateCoordinates(event) {
-    // pass event object, bound to mouse move with updat
+      // pass event object, bound to mouse move with updat
       this.x = event.clientX;
       this.y = event.clientY;
     },
@@ -70,7 +101,6 @@ export default {
     clear(event) {
       this.canvas.clear();
     },
-
   },
 };
 </script>
