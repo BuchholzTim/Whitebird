52d636974bf8837c090e8071c87f832cc95b6569
demo <demo7996@yahoo.de>
2020-12-07 15:09:20 +0100

    add resize Rect and mtiID for Group
    the mtiID don´t change after editing text

--- frontend/components/canvasTools/stickyNote.vue
+++ frontend/components/canvasTools/stickyNote.vue
@@ -15,10 +15,10 @@ export default {
   },
   data: () => ({
     overText: false,
-    overRect: false,
     editingText: false,
     groupItems: [],
     currentAngle: 0,
+    mtiIDGroup: null,
   }),
   mounted() {
     this.$nuxt.$on('stickyNoteTool', (payload) => {
@@ -29,8 +29,7 @@ export default {
     this.canvas.on('mouse:down', (options) => {
       if (
         this.editingText === true &&
-        this.overText === false &&
-        this.overRect === false
+        this.overText === false
       ) {
         this.groupItems[1].exitEditing();
 
@@ -45,28 +44,39 @@ export default {
         });
         const group = new fabric.Group([this.groupItems[0], this.groupItems[1]], {
           angle: this.currentAngle,
+          mtiID: this.mtiIDGroup,
         });
 
-        const invisibleControls = ['mt', 'mr', 'ml', 'mb'];
-
-        invisibleControls.forEach((side) => {
-          group.setControlVisible(side, false);
-        });
+        this.resetData();
+        this.addGroupSettings(group);
+        console.log(group.mtiID);
+        this.editingText = false;
 
         this.canvas.add(group);
-        this.groupAddonMouse(group);
-        this.editingText = false;
         this.canvas.requestRenderAll();
       }
     });
   },
   methods: {
-    groupAddonMouse(group) {
+
+    resetData() {
+      this.groupItems = [];
+      this.currentAngle = 0;
+      this.mtiIDoldGroup = null;
+    },
+    addGroupSettings(group) {
+      const invisibleControls = ['mt', 'mr', 'ml', 'mb'];
+
+      invisibleControls.forEach((side) => {
+        group.setControlVisible(side, false);
+      });
       group.on('mousedblclick', (options) => {
         console.log('Group DB Event');
         this.groupItems[0] = group.item(0);
         this.groupItems[1] = group.item(1);
         this.currentAngle = group.angle;
+        this.mtiIDGroup = group.mtiID;
+        console.log(group.mtiID);
         this.canvas.getActiveObject().toActiveSelection();
         this.canvas.setActiveObject(this.groupItems[1]);
         this.editingText = true;
@@ -89,6 +99,8 @@ export default {
         fill: 'rgb(0,0,0)',
         // fill: 'rgb(255,255,255)',
         fontFamily: 'Arial',
+        editingBorderColor: 'rgb(55, 71, 79)',
+        selectable: false,
         mtiID: v4(),
       });
       text.on('mouseover', (options) => {
@@ -104,24 +116,29 @@ export default {
         width: text.width + 30,
         height: text.height + 30,
         fill: 'rgb(55, 71, 79)',
+        selectable: false,
         shadow,
         mtiID: v4(),
       });
-      rect.on('mouseover', (options) => {
-        this.overRect = true;
-      });
-      rect.on('mouseout', (options) => {
-        this.overRect = false;
+
+      text.on('changed', () => {
+        console.log('Event changed Triggered');
+        // eslint-disable-next-line no-underscore-dangle
+        this.groupItems[0].left = this.groupItems[1].left - 15;
+        this.groupItems[0].top = this.groupItems[1].top - 15;
+        this.groupItems[0].width = this.groupItems[1].width + 30;
+        this.groupItems[0].height = this.groupItems[1].height + 30;
+        this.groupItems[0].dirty = true;
+        text.width = null;
+        this.canvas.requestRenderAll();
       });
 
-      const group = new fabric.Group([rect, text], {});
-      const invisibleControls = ['mt', 'mr', 'ml', 'mb'];
-
-      invisibleControls.forEach((side) => {
-        group.setControlVisible(side, false);
+      const group = new fabric.Group([rect, text], {
+        mtiID: v4(),
       });
 
-      this.groupAddonMouse(group);
+      this.resetData();
+      this.addGroupSettings(group);
 
       this.canvas.add(group);
       this.canvas.renderAll();
