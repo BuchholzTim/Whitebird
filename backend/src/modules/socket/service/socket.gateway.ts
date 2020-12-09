dcbce706be936f0b4af44e300a6de2b138766b9e
Marc Bollmann <contact@marcbollmann.de>
2020-12-09 16:41:48 +0100

    Better Logging

--- backend/src/modules/socket/service/socket.gateway.ts
+++ backend/src/modules/socket/service/socket.gateway.ts
@@ -18,30 +18,47 @@ export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGate
         this.logger.log('Initialized SocketGateway');
     }
 
-    handleConnection(client: Socket) {
-        this.logger.log('Client connected: ' + client.id)
+    handleConnection(client: Socket, message: { sender: string, room: string, message: string }) {
+        this.logger.log(`[connection] from client(${client.id})`);
     }
 
     handleDisconnect(client: Socket) {
-        this.logger.log('Client disconnected: ' + client.id);
+        this.logger.log(`[disconnection] from client(${client.id})`);
     }
 
     @SubscribeMessage('messageToServer')
     handleMessageToServer(client: Socket, message: { sender: string, room: string, message: string }) {
-        this.logger.log('messageToServer: ' + message);
+        this.logger.log(`[messageToServer] from ${message.sender}(${client.id}) to ${message.room} "${message.message}"`);
         this.webSocketServer.to(message.room).emit('messageToClient', message)
     }
 
+    /*
+    @SubscribeMessage('createCanvasObject')
+    handleCreateCanvasObject(client: Socket, message: { sender: string, room: string, message: string }) {
+        this.logger.log(`[messageToServer] from ${message.sender}(${client.id}) to ${message.room} "${message.message}"`);
+        this.webSocketServer.to(message.room).emit('messageToClient', message)
+    }
+    */
+
+    /*
+    @SubscribeMessage('changeCanvasObject')
+    handleChangeCanvasObject(client: Socket, message: {})
+    */
+
+    /*
+    @SubscribeMessage('deleteCanvasObject')
+    */
+
     @SubscribeMessage('joinWhiteboard')
     handleJoinRoom(client: Socket, message: { sender: string, room: string, message: string }) {
-        this.logger.log(`${client.id} joins ${message.room}`);
+        this.logger.log(`[joinWhiteboard] ${message.sender}(${client.id}) joins ${message.room}`);
         client.join(message.room);
         client.emit('joinedWhiteboard', message.room);
     }
 
     @SubscribeMessage('leaveWhiteboard')
     handleLeaveRoom(client: Socket, message: { sender: string, room: string, message: string }) {
-        this.logger.log(`${client.id} leaves ${message.room}`);
+        this.logger.log(`[leaveWhiteboard] ${message.sender}(${client.id}) leaves ${message.room}`);
         client.leave(message.room);
         client.emit('leftWhiteboard', message.room);
     }
