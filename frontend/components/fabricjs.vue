6ec2d49eee82c5300e09a15fac099d8f3d091924
demo <demo7996@yahoo.de>
2020-12-10 19:47:40 +0100

    with Key "entf" objects will be delete

--- frontend/components/fabricjs.vue
+++ frontend/components/fabricjs.vue
@@ -1,15 +1,16 @@
 <template>
   <div @mousemove="updateCoordinates">
-    <canvas id="canvas" width="1200px" height="850px"> </canvas> <br />
+    <div>
+      <canvas id="canvas" width="1200px" height="850px"> </canvas>
+    </div>
+    <br />
+    <button :style="{ color: drawingColor }" @click="drawingTool">Drawing</button>
     <button @click="rectangleTool">Rectangle</button>
-    <button @click="drawingTool">Drawing</button>
     <button @click="deleteAll">deleteAll</button>
-    <button @click="rubberTool">Rubber</button>
     <button @click="stickyNoteTool">Sticky</button>
     <client-only>
       <StickyNote :canvas="canvas"></StickyNote>
       <Drawing :canvas="canvas"></Drawing>
-      <Rubber :canvas="canvas"></Rubber>
     </client-only>
     <br />
     Coordinates: {{ x }} / {{ y }} <br />
@@ -22,7 +23,6 @@ import { v4 } from 'uuid';
 import { log } from 'util';
 import StickyNote from '~/components/canvasTools/stickyNote';
 import Drawing from '~/components/canvasTools/drawing';
-import Rubber from '~/components/canvasTools/rubber';
 
 let width;
 let height;
@@ -35,7 +35,6 @@ export default {
   components: {
     StickyNote,
     Drawing,
-    Rubber,
   },
   data: () => ({
     canvas: null,
@@ -45,6 +44,8 @@ export default {
     },
     x: 0,
     y: 0,
+    drawingMode: false,
+    drawingColor: '#ff0000',
   }),
   mounted() {
     console.log('Component created!');
@@ -52,17 +53,54 @@ export default {
     this.canvas.on('mouse:dblclick', (options) => {
       console.log('Canvas mouse:dblclick');
     });
+
+    // Add Window.Event for deleting Object with ENFT KEY
+    this.canvas.on('mouse:over', (event) => {
+      if (event.target === null) {
+        if (typeof window !== 'undefined') {
+          this.$nuxt.$emit('keydown_Event_Stop', false);
+        }
+      }
+    });
+    this.canvas.on('mouse:out', (event) => {
+      if (event.target === null) {
+        if (typeof window !== 'undefined') {
+          this.$nuxt.$emit('keydown_Event_Stop', true);
+        }
+      }
+    });
+    this.$nuxt.$on('keydown_Event_Stop', (payload) => {
+      if (payload === true) {
+        window.removeEventListener('keydown', this.deleteObject);
+      } else {
+        window.addEventListener('keydown', this.deleteObject);
+      }
+    });
   },
 
   methods: {
+    deleteObject(event) {
+      if (event.key === 'Delete') {
+        this.canvas.getActiveObjects().forEach((obj) => {
+          // eslint-disable-next-line prefer-template
+          console.log('delete: ' + obj.type + 'Object');
+          this.canvas.remove(obj);
+        });
+
+        this.canvas.discardActiveObject().renderAll();
+      }
+    },
     stickyNoteTool() {
       this.$nuxt.$emit('stickyNoteTool');
     },
     drawingTool() {
+      this.drawingMode = !this.drawingMode;
+      if (this.drawingMode === true) {
+        this.drawingColor = '#2a8108';
+      } else {
+        this.drawingColor = '#ff0000';
+      }
       this.$nuxt.$emit('drawingTool');
-    },
-    rubberTool(event) {
-      this.$nuxt.$emit('rubberTool');
     },
     rectangleTool(event) {
       const rect = new fabric.Rect({
@@ -76,6 +114,7 @@ export default {
         console.log('rect event');
       });
       this.canvas.add(rect).setActiveObject(rect);
+      console.log(rect);
       this.canvas.renderAll();
     },
 
@@ -88,7 +127,6 @@ export default {
     deleteAll(event) {
       this.canvas.clear();
     },
-
   },
 };
 </script>
@@ -96,5 +134,8 @@ export default {
 <style>
 #canvas {
   border: 1px solid;
+}
+#Test {
+  color: #2a8108;
 }
 </style>
