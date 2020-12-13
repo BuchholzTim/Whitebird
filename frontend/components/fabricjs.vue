9a1f27eb080d304b78f0fd3aebb4f7d39b38e297
Tim Buchholz <buchholz.tim@gmx.de>
2020-12-13 12:44:05 +0100

    Moved Delete-Functionality into separate Tool-File

--- frontend/components/fabricjs.vue
+++ frontend/components/fabricjs.vue
@@ -7,6 +7,7 @@
       <StickyNoteTool :canvas="canvas"></StickyNoteTool>
       <DrawingTool :canvas="canvas"></DrawingTool>
       <ClearTool :canvas="canvas"></ClearTool>
+      <DeleteTool :canvas="canvas"></DeleteTool>
     </client-only>
   </div>
 </template>
@@ -18,6 +19,7 @@ import DrawingTool from '~/components/canvasTools/DrawingTool';
 import RectangleTool from '~/components/canvasTools/RectangleTool';
 import CircleTool from '~/components/canvasTools/CircleTool';
 import ClearTool from '~/components/canvasTools/ClearTool';
+import DeleteTool from '~/components/canvasTools/DeleteTool';
 import customEvents from '~/utils/customEvents';
 
 let width;
@@ -34,6 +36,7 @@ export default {
     RectangleTool,
     CircleTool,
     ClearTool,
+    DeleteTool,
   },
   data: () => ({
     canvas: null,
@@ -41,52 +44,7 @@ export default {
   mounted() {
     console.log('Component created!');
     this.canvas = new fabric.Canvas('canvas');
-
-    // Activate Event-Listener for 'Delete-Key', when Mouse is over canvas
-    this.canvas.on('mouse:over', (event) => {
-      if (event.target === null) {
-        this.activateRemoveObjectEventListener();
-      }
-    });
-    // Remove Event-Listener for 'Delete-Key', when Mouse is outside Canvas
-    this.canvas.on('mouse:out', (event) => {
-      if (event.target === null) {
-        this.deactivateRemoveObjectEventListener();
-      }
-    });
-
-    this.$nuxt.$on(
-      customEvents.canvasTools.setRemoveObjectEventListener,
-      (payload) => {
-        if (payload) {
-          this.activateRemoveObjectEventListener();
-        } else {
-          this.deactivateRemoveObjectEventListener();
-        }
-      },
-    );
   },
-  methods: {
-    // Removes all Selected Objects on the Canvas on 'Delete'-Key
-    deleteObject(event) {
-      if (event.key === 'Delete') {
-        this.canvas.getActiveObjects().forEach((obj) => {
-          console.log(`delete: ${obj.type}Object`);
-          this.canvas.remove(obj);
-        });
-        this.canvas.discardActiveObject().renderAll();
-      }
-    },
-    activateRemoveObjectEventListener() {
-      if (typeof window !== 'undefined') {
-        window.addEventListener('keydown', this.deleteObject);
-      }
-    },
-    deactivateRemoveObjectEventListener() {
-      if (typeof window !== 'undefined') {
-        window.removeEventListener('keydown', this.deleteObject);
-      }
-    },
-  },
+  methods: {},
 };
 </script>
