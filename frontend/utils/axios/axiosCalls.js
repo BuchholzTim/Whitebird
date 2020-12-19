83e47b504968179afd418c9509a91dca88002b69
Tim Buchholz <buchholz.tim@gmx.de>
2020-12-19 17:59:49 +0100

    AxiosCalls now reflect the new API

--- frontend/utils/axios/axiosCalls.js
+++ frontend/utils/axios/axiosCalls.js
@@ -1,17 +1,44 @@
+// Create a New Whiteboard
 function createWhiteboard($axios) {
   return $axios.$get('/whiteboard/create');
 }
 
+// Join an Existing Whiteboard
 function joinWhiteboard($axios, canvasID) {
   return $axios.$get(`/whiteboard/${canvasID}/join`);
 }
 
-function deleteCanvasObject($axios, canvasID, canvasObject) {
+// Create a new Canvas-Object on a Whiteboard
+function createCanvasObject($axios, canvasID, canvasObject) {
   return $axios
-    .$delete(`/whiteboard/${canvasID}/canvas/delete`, {
+    .$post(`/whiteboard/${canvasID}/canvas/object`, {
       object: canvasObject,
     })
     .then((res) => console.log(res));
 }
 
-module.exports = { createWhiteboard, joinWhiteboard, deleteCanvasObject };
+// Delete an Existing Canvas-Object
+function deleteCanvasObject($axios, canvasID, canvasObject) {
+  return $axios
+    .$delete(`/whiteboard/${canvasID}/canvas/object`, {
+      object: canvasObject,
+    })
+    .then((res) => console.log(res));
+}
+
+// Update an Existing Canvas-Object
+function updateCanvasObject($axios, canvasID, canvasObject) {
+  return $axios
+    .$put(`/whiteboard/${canvasID}/canvas/object`, {
+      object: canvasObject,
+    })
+    .then((res) => console.log(res));
+}
+
+module.exports = {
+  createWhiteboard,
+  joinWhiteboard,
+  createCanvasObject,
+  updateCanvasObject,
+  deleteCanvasObject,
+};
