bdb403b11af2061371e27e2bc0e3a38ed117c037
Tim Buchholz <buchholz.tim@gmx.de>
2020-12-19 22:13:37 +0100

    Moved AxiosCalls to Store

--- frontend/store/README.md
+++ frontend/store/README.md
@@ -8,3 +8,7 @@ Vuex Store option is implemented in the Nuxt.js framework.
 Creating a file in this directory automatically activates the option in the framework.
 
 More information about the usage of this directory in [the documentation](https://nuxtjs.org/guide/vuex-store).
+
+---
+
+Better Information in [A better Tutorial](https://itnext.io/efficiently-understanding-and-using-nuxt-vuex-7905eb8858d6)
---
+++ frontend/store/canvas.js
@@ -0,0 +1,89 @@
+const customToJSON = (canvasObject) => {
+  // Axios will call 'toJSON' before sending, as we cannot actually send an Object
+  // toJson(), will remove our custom mtiID, so we have to Re-Add it afterwards.
+  const asJSON = canvasObject.toJSON();
+  asJSON.mtiID = canvasObject.mtiID;
+
+  return asJSON;
+};
+
+const CANVASID_NOT_DEFINED = 'Canvas ID is not defined!';
+
+/* eslint-disable no-shadow */
+export const state = () => ({
+  id: null,
+});
+
+export const mutations = {
+  set(state, id) {
+    state.id = id;
+  },
+};
+
+export const actions = {
+  async createCanvas({ commit }) {
+    await this.$axios.get('/whiteboard/create').then((res) => {
+      if (res.status === 200) {
+        const canvasID = res.data._id;
+        commit('set', canvasID);
+      }
+    });
+  },
+  async joinCanvas({ commit }, canvasID) {
+    if (state.id) {
+      console.error(CANVASID_NOT_DEFINED);
+      return;
+    }
+    await this.$axios.get(`whiteboard/${canvasID}/join`).then((res) => {
+      if (res.status === 200) {
+        commit('set', canvasID);
+      }
+    });
+  },
+  // Create a new Canvas-Object on a Whiteboard
+  async createCanvasObject({ state }, canvasObject) {
+    if (!state.id) {
+      console.error(CANVASID_NOT_DEFINED);
+      return;
+    }
+
+    const asJSON = customToJSON(canvasObject);
+    await this.$axios
+      .post(`/whiteboard/${state.id}/canvas/object`, {
+        object: asJSON,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  },
+
+  // Delete an Existing Canvas-Object
+  async deleteCanvasObject({ state }, canvasObject) {
+    if (!state.id) {
+      console.error(CANVASID_NOT_DEFINED);
+      return;
+    }
+
+    const asJSON = customToJSON(canvasObject);
+    await this.$axios
+      .$delete(`/whiteboard/${state.id}/canvas/object`, {
+        object: asJSON,
+      })
+      .then((res) => console.log(res));
+  },
+
+  // Update an Existing Canvas-Object
+  async updateCanvasObject({ state }, canvasObject) {
+    if (!state.id) {
+      console.error(' Canvas ID is not defined!');
+      return;
+    }
+
+    const asJSON = customToJSON(canvasObject);
+    await this.$axios
+      .$put(`/whiteboard/${state.id}/canvas/object`, {
+        object: asJSON,
+      })
+      .then((res) => console.log(res));
+  },
+};
