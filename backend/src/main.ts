c961dd27ec59f7189e6fb59f1e15a1ec86ee1ecf
Tim Buchholz <tim.buchholz@protonmail.com>
2021-01-22 12:10:21 +0100

    Docker works, Backend Currently in Dev-Only

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
   });
 
