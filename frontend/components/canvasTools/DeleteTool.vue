a593c1aec58500c7a5192ca13a0128cac18043a2
Benny Lai <howo0707@gmail.com>
2021-03-15 16:54:33 +0800

    Test (#5)
    * Used keyboard to demonstrate the object layer method.
    
    * Used keyboard to demonstrate the object lock movement method.
    
    * Edited the comment at DeleteTool.
    
    * Used keyboard to demonstrate the object undo and redo method.

--- frontend/components/canvasTools/DeleteTool.vue
+++ frontend/components/canvasTools/DeleteTool.vue
@@ -12,6 +12,12 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      isRedoing: false,
+      h: []
+    }
+  },
   mounted() {
     // Activate Event-Listener for 'Delete-Key', when Mouse is over canvas
     this.canvas.on('mouse:over', (event) => {
@@ -36,6 +42,13 @@ export default {
         }
       },
     );
+
+    this.canvas.on('object:added', () => {
+      if (!this.isRedoing) {
+        this.h = [];
+      }
+      this.isRedoing = false;
+    });
   },
   methods: {
     // Removes all Selected Objects on the Canvas on 'Delete'-Key
@@ -48,6 +61,62 @@ export default {
           }
         });
         this.canvas.discardActiveObject().renderAll();
+      } else if (event.key === 'f') {
+        this.canvas.getActiveObjects().forEach((obj) => {
+          // If object are not selectable -> not movable
+          if (obj.selectable) {
+            this.canvas.bringToFront(obj)
+          }
+        });
+        this.canvas.discardActiveObject().renderAll();
+      } else if (event.key ==='g') {
+        this.canvas.getActiveObjects().forEach((obj) => {
+          // If object are not selectable -> not movable
+          if (obj.selectable) {
+            this.canvas.bringForward(obj)
+          }
+        });
+        this.canvas.discardActiveObject().renderAll();
+      } else if (event.key === 'b') {
+        this.canvas.getActiveObjects().forEach((obj) => {
+          // If object are not selectable -> not movable
+          if (obj.selectable) {
+            this.canvas.sendToBack(obj)
+          }
+        });
+        this.canvas.discardActiveObject().renderAll();
+      } else if (event.key === 'n') {
+        this.canvas.getActiveObjects().forEach((obj) => {
+          // If object are not selectable -> not movable
+          if (obj.selectable) {
+            this.canvas.sendBackwards(obj)
+          }
+        });
+        this.canvas.discardActiveObject().renderAll();
+      } else if (event.key === 'p') {
+        this.canvas.getActiveObjects().forEach((obj) => {
+          // If object are not selectable -> not being pinned
+          if (obj.selectable) {
+            obj.lockMovementX = true
+            obj.lockMovementY = true
+          }
+        });
+        this.canvas.discardActiveObject().renderAll();
+      } else if (event.key === 'o') {
+        this.canvas.getActiveObjects().forEach((obj) => {
+          // If object are not selectable -> not being pinned
+          if (obj.selectable) {
+            obj.lockMovementX = false
+            obj.lockMovementY = false
+          }
+        });
+        this.canvas.discardActiveObject().renderAll();
+      } else if (event.key === 'u') {
+        this.undo()
+        this.canvas.discardActiveObject().renderAll();
+      } else if (event.key === 'i') {
+        this.redo()
+        this.canvas.discardActiveObject().renderAll();
       }
     },
     activateRemoveObjectEventListener() {
@@ -58,6 +127,17 @@ export default {
     deactivateRemoveObjectEventListener() {
       if (typeof window !== 'undefined') {
         window.removeEventListener('keydown', this.deleteObject);
+      }
+    },
+    undo() {
+      if (this.canvas._objects.length > 0) {
+        this.h.push(this.canvas._objects.pop());
+      }
+    },
+    redo() {
+      if (this.h.length > 0) {
+        this.isRedoing = true;
+        this.canvas.add(this.h.pop());
       }
     },
   },
