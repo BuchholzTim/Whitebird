0a6872f458000a1732acbbd8543480bcad1151ff
Benny LAI <howo0707@gmail.com>
2021-03-30 04:02:00 +0000

    Changed target to static.

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   },
 
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'server',
+  target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
